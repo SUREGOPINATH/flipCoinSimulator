3f33c00e0b48fc8e6e893236a4b987834019908f
SURE GOPINATH <suregopinath@gmail.com>
2020-10-08 12:30:22 +0530

    toss checker is done

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,3 +2,19 @@
 
 echo "Welcome to flip coin simulator"
 
+function tossChecker() {
+        toss=$(($RANDOM%2))
+        echo $toss
+}
+
+function results() {
+        if [ $headCount -gt $tailCount ]
+        then
+                echo "heads won "
+        else
+                echo "tails won"
+        fi
+}
+
+tossChecker
+results
