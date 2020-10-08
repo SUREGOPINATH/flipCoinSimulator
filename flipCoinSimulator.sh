cb621160dcd0776c9d1e356c4a7198fa4ebf11f7
SURE GOPINATH <suregopinath@gmail.com>
2020-10-08 12:34:50 +0530

    multiple time toss check and find who won is done

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,6 +2,13 @@
 
 echo "Welcome to flip coin simulator"
 
+#variables
+heads=0
+tails=1
+headCount=0
+tailCount=0
+count=0
+
 function tossChecker() {
         toss=$(($RANDOM%2))
         echo $toss
@@ -16,5 +23,17 @@ function results() {
         fi
 }
 
-tossChecker
+while [ $count -ne 5 ]
+do
+        toss="$(tossChecker)"
+        case "$toss" in
+                0)
+                        headCount=$(($headCount+1))
+                        ;;
+                1)
+                        tailCount=$(($tailCount+1))
+                        ;;
+        esac
+        count=$(($count+1))
+done
 results
